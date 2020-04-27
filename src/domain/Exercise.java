82b9176dd8aa79c8f34f3879a43f25ae0daa9bb7
Ilya <mokienkoilya@gmail.com>
2020-04-27 17:49:25 +0300

    done

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,23 @@
 package domain;
 
+import java.util.Arrays;
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    /**
+     *The function takes an array, sorts it in ascending order. Then, with the algorithm, I look for a real second number in magnitude, that is, in the array {1, 2, 2}, the second largest will be 1.
+     * @param arr input array
+     * @return second largest number
+     */
+    public static int Calculate(int[] arr){
+        Arrays.sort(arr);
+
+        int i = 1;
+        while(arr[arr.length - (i+1)] == arr[arr.length - i]){
+            i++;
+            if(arr[arr.length - (i+1)] != arr[arr.length - i]){
+                return arr[arr.length - (i+1)];
+            }
+        }
+        return arr[arr.length - 2];
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -2,11 +2,19 @@ package test;
 
 import domain.Exercise;
 
+import java.util.Scanner;
+
 public class TestResult {
 
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите величину массива: ");
+        int N = in.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Введите " + (i+1) + " элемент массива: ");
+            arr[i] = in.nextInt();
+        }
+        System.out.println("Второе по величене число = "+Exercise.Calculate(arr));
     }
 }
